75dfefdfb73c8fc73e4ed22fa34b5a99d9d6a912
Felix <sontug9xx@gmail.com>
2025-09-15 23:08:06 +0700

    SPOR14/feat/updateSubcription

--- src/subcription/subcription.controller.ts
+++ src/subcription/subcription.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { SubcriptionService } from './subcription.service';
 import { CreateSubscriptionDto } from './DTO/subcriptionDto';
 import { ApiBearerAuth, ApiBody } from '@nestjs/swagger';
@@ -6,6 +13,7 @@ import { Roles } from 'src/common/decorators/role.decorator';
 import { Role as RoleEnum } from 'src/common/enum/Role';
 import { RolesGuard } from 'src/common/guards/role.guard';
 import { Request } from '@nestjs/common';
+import { UpdateSubscriptionDto } from './DTO/updateSubcriptionDto';
 
 @Controller('subcription')
 export class SubcriptionController {
@@ -26,8 +34,42 @@ export class SubcriptionController {
     },
   })
   @Post()
-  async create(@Body() createSubscriptionDto: CreateSubscriptionDto, @Request() req) {
+  async create(
+    @Body() createSubscriptionDto: CreateSubscriptionDto,
+    @Request() req,
+  ) {
     const loggedInUser = req.user;
-    return this.subcriptionService.createSubscription(createSubscriptionDto, loggedInUser);
+    return this.subcriptionService.createSubscription(
+      createSubscriptionDto,
+      loggedInUser,
+    );
+  }
+
+  @ApiBearerAuth('access-token')
+  @UseGuards(RolesGuard)
+  @Roles(RoleEnum.ADMIN)
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: { type: 'string', example: 'Basketball' },
+        price: { type: 'number', example: 9.99 },
+        duration: { type: 'number', example: 30 },
+        description: { type: 'string', example: 'A popular sport' },
+      },
+    },
+  })
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateSubscriptionDto: UpdateSubscriptionDto,
+    @Request() req,
+  ) {
+    const loggedInUser = req.user;
+    return this.subcriptionService.updateSubscription(
+      id,
+      updateSubscriptionDto,
+      loggedInUser,
+    );
   }
 }
--- src/subcription/subcription.service.ts
+++ src/subcription/subcription.service.ts
@@ -4,6 +4,7 @@ import { Subscription } from './entities/subscription.entity';
 import { Repository } from 'typeorm';
 import { CreateSubscriptionDto } from './DTO/subcriptionDto';
 import { User } from 'src/user/entities/user.entity';
+import { UpdateSubscriptionDto } from './DTO/updateSubcriptionDto';
 
 @Injectable()
 export class SubcriptionService {
@@ -11,8 +12,9 @@ export class SubcriptionService {
     @InjectRepository(Subscription)
     private readonly subscriptionRepository: Repository<Subscription>,
   ) {}
-  async createSubscription(createSubscriptionDto: CreateSubscriptionDto,
-    owner: User
+  async createSubscription(
+    createSubscriptionDto: CreateSubscriptionDto,
+    owner: User,
   ) {
     const { name, price, duration, description } = createSubscriptionDto;
     const newSubscription = this.subscriptionRepository.create({
@@ -21,8 +23,11 @@ export class SubcriptionService {
       duration,
       description,
     });
-    if(owner.role !== 'ADMIN'){
-      throw new HttpException({ message: 'You don\'t have permission to create a subscription' }, HttpStatus.FORBIDDEN);
+    if (owner.role !== 'ADMIN') {
+      throw new HttpException(
+        { message: "You don't have permission to create a subscription" },
+        HttpStatus.FORBIDDEN,
+      );
     }
     try {
       const result = await this.subscriptionRepository.save(newSubscription);
@@ -31,6 +36,42 @@ export class SubcriptionService {
       const err = error as Error;
       throw new HttpException(
         { message: err.message || 'Error creating subscription' },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+  async updateSubscription(
+    id: string,
+    updateSubscriptionDto: UpdateSubscriptionDto,
+    owner: User,
+  ) {
+    const { name, price, duration, description } = updateSubscriptionDto;
+    const subscription = await this.subscriptionRepository.findOne({
+      where: { subscriptionId: id },
+    });
+    if (!subscription) {
+      throw new HttpException(
+        { message: 'Subscription not found' },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    if (owner.role !== 'ADMIN') {
+      throw new HttpException(
+        { message: "You don't have permission to update this subscription" },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+    try {
+      subscription.name = name;
+      subscription.price = price;
+      subscription.duration = duration;
+      subscription.description = description;
+      const result = await this.subscriptionRepository.save(subscription);
+      return result;
+    } catch (error) {
+      const err = error as Error;
+      throw new HttpException(
+        { message: err.message || 'Error updating subscription' },
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
