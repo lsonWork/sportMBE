0715bb887bdb8fc0f7eb6c23d5efae1621bd10cf
lson <sonla@btc-studio.io>
2025-09-23 08:30:57 +0700

    fix import repo to module

--- src/court/court.module.ts
+++ src/court/court.module.ts
@@ -6,9 +6,10 @@ import { CourtImage } from './entities/courtImage.entity';
 import { SportType } from 'src/sport-type/entities/sportType.entity';
 import { CourtController } from './court.admin.controller';
 import { CourtPublicController } from './court.public.controller';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Court, CourtImage, SportType])],
+  imports: [TypeOrmModule.forFeature([Court, CourtImage, SportType, User])],
   providers: [CourtService],
   controllers: [CourtController, CourtPublicController],
 })
