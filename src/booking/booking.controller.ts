41e43aab985d3782f4472f9ee89d30d0e534c084
Felix <sontug9xx@gmail.com>
2025-10-09 03:02:32 +0700

    migration/Addcoolum

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -74,6 +74,10 @@ export class BookingController {
           },
           example: ['d290f1ee-6c54-4b01-90e6-d701748f0851'],
         },
+        notes: {
+          type: 'string',
+          example: 'This is a note for the booking order.',
+        },
       },
     },
   })
